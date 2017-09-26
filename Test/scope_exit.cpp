db937df56c34a9cb985843bccb98cf39d87dff0b
Yuu Shimizu <p@yuushimizu.com>
2017-09-26 21:37:28 +0900

    add scope_exit test

---
+++ Test/scope_exit.cpp
@@ -0,0 +1,19 @@
+#include <gtest/gtest.h>
+#include "ufo/scope_exit.hpp"
+
+using namespace ufo;
+
+namespace {
+    TEST(ScopeExitTest, Destructor) {
+        int n = 10;
+        {
+            auto add = scope_exit([&n]() mutable {
+                ++n;
+            });
+            ASSERT_EQ(10, n);
+        }
+        ASSERT_EQ(11, n);
+    }
+}
+
+

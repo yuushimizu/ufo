deff0a5e45128c3fb6dee5a0c45cab6d57cec5b2
Yuu Shimizu <p@yuushimizu.com>
2017-09-27 17:28:14 +0900

    add tests

--- Test/partial.cpp
+++ Test/partial.cpp
@@ -78,6 +78,18 @@ namespace {
         ASSERT_EQ(~50, f(50));
     }
     
+    TEST(PartialTest, NotEqualLeft) {
+        auto f = (_ != 10);
+        ASSERT_TRUE(f(8));
+        ASSERT_FALSE(f(10));
+    }
+    
+    TEST(PartialTest, NotEqualRight) {
+        auto f = (55 != _);
+        ASSERT_TRUE(f(66));
+        ASSERT_FALSE(f(55));
+    }
+    
     TEST(PartialTest, ModuloLeft) {
         auto f = (_ % 2);
         ASSERT_EQ(1, f(7));
@@ -86,6 +98,16 @@ namespace {
     TEST(PartialTest, ModuloRight) {
         auto f = (5 % _);
         ASSERT_EQ(2, f(3));
+    }
+    
+    TEST(PartialTest, BitwiseAndLeft) {
+        auto f = (_ & 2);
+        ASSERT_EQ(100 & 2, f(100));
+    }
+    
+    TEST(PartialTest, BitwiseAndRight) {
+        auto f = (3 & _);
+        ASSERT_EQ(3 & 5, f(5));
     }
     
     TEST(PartialTest, PlusLeft) {

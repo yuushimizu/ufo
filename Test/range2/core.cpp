90ee4d1b6a3101ff3e75f0e6c57745d8672f4bfa
Yuu Shimizu <p@yuushimizu.com>
2017-10-02 00:23:46 +0900

    add first tests

--- Test/range2/core.cpp
+++ Test/range2/core.cpp
@@ -17,4 +17,15 @@ namespace {
         static_assert(std::is_same_v<int, decltype(std::vector<int> {10, 20, 30} | first)>);
         ASSERT_EQ(10, (std::vector<int> {10, 20, 30} | first));
     }
+    
+    TEST(ContainerWrapperTest, FirstLValueCall) {
+        std::vector<int> v {10, 20, 30};
+        static_assert(std::is_same_v<int &, decltype(first(v))>);
+        ASSERT_EQ(&v[0], &first(v));
+    }
+    
+    TEST(ContainerWrapperTest, FirstRValueCall) {
+        static_assert(std::is_same_v<int, decltype(first(std::vector<int> {10, 20, 30}))>);
+        ASSERT_EQ(10, first(std::vector<int> {10, 20, 30}));
+    }
 }

a5db9b578024a426e859cc58ca1df18fbd1df1c1
Yuu Shimizu <p@yuushimizu.com>
2017-10-02 00:36:19 +0900

    add rest and is_empty

--- Test/range2/core.cpp
+++ Test/range2/core.cpp
@@ -7,25 +7,79 @@
 using namespace ufo;
 
 namespace {
-    TEST(ContainerWrapperTest, FirstLValue) {
+    TEST(FirstTest, LValue) {
         std::vector<int> v {10, 20, 30};
         static_assert(std::is_same_v<int &, decltype(v | first)>);
         ASSERT_EQ(&v[0], &(v | first));
     }
     
-    TEST(ContainerWrapperTest, FirstRValue) {
+    TEST(FirstTset, RValue) {
         static_assert(std::is_same_v<int, decltype(std::vector<int> {10, 20, 30} | first)>);
         ASSERT_EQ(10, (std::vector<int> {10, 20, 30} | first));
     }
     
-    TEST(ContainerWrapperTest, FirstLValueCall) {
+    TEST(FirstTest, LValueCall) {
         std::vector<int> v {10, 20, 30};
         static_assert(std::is_same_v<int &, decltype(first(v))>);
         ASSERT_EQ(&v[0], &first(v));
     }
     
-    TEST(ContainerWrapperTest, FirstRValueCall) {
+    TEST(FirstTest, RValueCall) {
         static_assert(std::is_same_v<int, decltype(first(std::vector<int> {10, 20, 30}))>);
         ASSERT_EQ(10, first(std::vector<int> {10, 20, 30}));
+    }
+    
+    TEST(RestTest, LValue) {
+        std::vector<int> v {10, 20, 30};
+        ASSERT_EQ(&v[1], &(v | rest | first));
+    }
+    
+    TEST(RestTest, RValue) {
+        ASSERT_EQ(20, (std::vector<int> {10, 20, 30} | rest | first));
+    }
+    
+    TEST(RestTest, LValueCall) {
+        std::vector<int> v {10, 20, 30};
+        ASSERT_EQ(&v[1], &first(rest(v)));
+    }
+    
+    TEST(RestTest, RValueCall) {
+        ASSERT_EQ(20, first(rest(std::vector<int> {10, 20, 30})));
+    }
+    
+    TEST(IsEmptyTest, LValueTrue) {
+        std::vector<int> v {};
+        ASSERT_TRUE(v | is_empty);
+    }
+    
+    TEST(IsEmptyTest, RValueTrue) {
+        ASSERT_TRUE((std::vector<int> {} | is_empty));
+    }
+    
+    TEST(IsEmptyTest, LValueCallTrue) {
+        std::vector<int> v {};
+        ASSERT_TRUE(is_empty(v));
+    }
+    
+    TEST(IsEmptyTest, RValueCallTrue) {
+        ASSERT_TRUE(is_empty(std::vector<int> {}));
+    }
+    
+    TEST(IsEmptyTest, LValueFalse) {
+        std::vector<int> v {10, 20, 30};
+        ASSERT_FALSE(v | is_empty);
+    }
+    
+    TEST(IsEmptyTest, RValueFalse) {
+        ASSERT_FALSE((std::vector<int> {10, 20, 30} | is_empty));
+    }
+    
+    TEST(IsEmptyTest, LValueCallFalse) {
+        std::vector<int> v {10, 20, 30};
+        ASSERT_FALSE(is_empty(v));
+    }
+    
+    TEST(IsEmptyTest, RValueCallFalse) {
+        ASSERT_FALSE(is_empty(std::vector<int> {10, 20, 30}));
     }
 }
--- ufo/range2/core.hpp
+++ ufo/range2/core.hpp
@@ -7,6 +7,16 @@ namespace ufo {
     constexpr auto first = range_operator([](auto &&range) constexpr -> decltype(auto) {
         return std::forward<decltype(range)>(range).first();
     });
+    
+    constexpr auto rest = range_operator([](auto &&range) constexpr -> decltype(auto) {
+        auto result = std::forward<decltype(range)>(range);
+        result.pop();
+        return result;
+    });
+    
+    constexpr auto is_empty = range_operator([](const auto &range) constexpr -> bool {
+        return range.empty();
+    });
 }
 
 #endif

1aaa7bd0ce72b2f58af5737a4ab01562ab0b7d0e
Yuu Shimizu <p@yuushimizu.com>
2017-10-02 17:58:30 +0900

    add take tests

--- Test/range2/take.cpp
+++ Test/range2/take.cpp
@@ -9,14 +9,38 @@ using namespace ufo;
 namespace {
     TEST(TakeTest, LValue) {
         std::vector<int> v {10, 20, 30};
-        auto cw = v | take(2);
-        static_assert(std::is_same_v<int &, decltype(cw | first)>);
-        ASSERT_FALSE(cw | is_empty);
-        ASSERT_EQ(&v[0], &(cw | first));
-        cw = std::move(cw) | rest;
-        ASSERT_FALSE(cw | is_empty);
-        ASSERT_EQ(&v[1], &(cw | first));
-        cw = std::move(cw) | rest;
-        ASSERT_TRUE(cw | is_empty);
+        auto r = v | take(2);
+        static_assert(std::is_same_v<int &, decltype(r | first)>);
+        ASSERT_FALSE(r | is_empty);
+        ASSERT_EQ(&v[0], &(r | first));
+        r = std::move(r) | rest;
+        ASSERT_FALSE(r | is_empty);
+        ASSERT_EQ(&v[1], &(r | first));
+        r = std::move(r) | rest;
+        ASSERT_TRUE(r | is_empty);
+    }
+    
+    TEST(TakeTest, RValue) {
+        auto r = std::vector<int> {10, 20, 30} | take(2);
+        static_assert(std::is_same_v<int, decltype(r | first)>);
+        ASSERT_FALSE(r | is_empty);
+        ASSERT_EQ(10, r | first);
+        r = std::move(r) | rest;
+        ASSERT_FALSE(r | is_empty);
+        ASSERT_EQ(20, r | first);
+        r = std::move(r) | rest;
+        ASSERT_TRUE(r | is_empty);
+    }
+    
+    TEST(TakeTest, Empty) {
+        auto r = std::vector<int> {} | take(5);
+        ASSERT_TRUE(r | is_empty);
+    }
+    
+    TEST(TakeTest, Over) {
+        auto r = std::vector<int> {10} | take(5);
+        ASSERT_FALSE(r | is_empty);
+        r = std::move(r) | rest;
+        ASSERT_TRUE(r | is_empty);
     }
 }

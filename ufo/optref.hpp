ddf47843991fb3780ed7929d3d3908af0b26a8d3
Yuu Shimizu <p@yuushimizu.com>
2017-10-07 01:49:33 +0900

    add noexcept to optref

--- ufo/optref.hpp
+++ ufo/optref.hpp
@@ -8,62 +8,62 @@ namespace ufo {
     template <typename T>
     class optref final {
     public:
-        constexpr optref() : optional_ {} {
+        constexpr optref() noexcept : optional_ {} {
         }
         
-        constexpr optref(T &reference) : optional_(std::ref(reference)) {
+        constexpr optref(T &reference) noexcept : optional_(std::ref(reference)) {
         }
         
-        constexpr optref(std::experimental::nullopt_t) : optional_(std::experimental::nullopt) {
+        constexpr optref(std::experimental::nullopt_t) noexcept : optional_(std::experimental::nullopt) {
         }
         
-        ~optref() = default;
+        ~optref() noexcept = default;
         
         constexpr optref(const optref &) = default;
         
-        constexpr optref(optref &&) = default;
+        constexpr optref(optref &&) noexcept = default;
         
         template <typename U>
         constexpr optref(const optref<U> &other) : optref(*other) {
         }
         
-        constexpr optref &operator=(const optref &) = default;
+        constexpr optref &operator=(const optref &) noexcept = default;
         
-        constexpr optref &operator=(optref &&) = default;
+        constexpr optref &operator=(optref &&) noexcept = default;
         
-        constexpr optref &operator=(T &reference) {
+        constexpr optref &operator=(T &reference) noexcept {
             optional_ = std::ref(reference);
             return *this;
         }
         
-        constexpr optref &operator=(std::experimental::nullopt_t) {
-            optional_ =std::experimental:: nullopt;
+        constexpr optref &operator=(std::experimental::nullopt_t) noexcept {
+            optional_ = std::experimental::nullopt;
         }
         
         template <typename U>
-        constexpr optref &operator=(const optref<U> &other) {
+        constexpr optref &operator=(const optref<U> &other) noexcept {
             *this = *other;
             return *this;
         }
         
         template <typename U>
-        constexpr void emplace(U &reference) {
+        constexpr void emplace(U &reference) noexcept {
             optional_.emplace(reference);
         }
         
-        constexpr void reset() {
+        constexpr void reset() noexcept {
             optional_ = std::experimental::nullopt;
         }
         
-        constexpr operator bool() const {
+        constexpr operator bool() const noexcept {
             return static_cast<bool>(optional_);
         }
         
-        constexpr T *operator->() const {
+        constexpr T *operator->() const noexcept {
             return &optional_->get();
         }
         
-        constexpr T &operator*() const {
+        constexpr T &operator*() const noexcept {
             return optional_->get();
         }
         
@@ -74,12 +74,12 @@ namespace ufo {
     };
     
     template <typename LHS, typename RHS>
-    constexpr bool operator==(const optref<LHS> &lhs, const optref<RHS> &rhs) {
+    constexpr bool operator==(const optref<LHS> &lhs, const optref<RHS> &rhs) noexcept {
         return lhs.optional_ == rhs.optional_;
     }
     
     template <typename LHS, typename RHS>
-    constexpr bool operator!=(const optref<LHS> &lhs, const optref<RHS> &rhs) {
+    constexpr bool operator!=(const optref<LHS> &lhs, const optref<RHS> &rhs) noexcept {
         return !(lhs == rhs);
     }
 }

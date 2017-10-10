9d8a892fc0eae4cacc4bd8a819f8d0fd220abb6d
Yuu Shimizu <p@yuushimizu.com>
2017-10-10 17:12:15 +0900

    add tests for option.map

--- Test/option.cpp
+++ Test/option.cpp
@@ -66,4 +66,50 @@ namespace {
     TEST(OptionTest, ValueMapRValueNullopt) {
         ASSERT_EQ(nullopt, option<int> {}.map([](int &&n) {return n * 3;}));
     }
+    
+    TEST(OptionTest, RefMapLValue) {
+        int x = 10;
+        option<int &> o(x);
+        decltype(auto) r = o.map([](int &n) {return n * 2;});
+        static_assert(std::is_same_v<option<int>, decltype(r)>);
+        ASSERT_EQ(10, *o);
+        ASSERT_EQ(20, *r);
+    }
+    
+    TEST(OptionTest, RefMapLValueNullopt) {
+        option<int &> o {};
+        ASSERT_EQ(nullopt, o.map([](int &n) {return n * 2;}));
+    }
+    
+    TEST(OptionTest, RefMapConstLValue) {
+        int x = 5;
+        const option<int &> o(x);
+        decltype(auto) r = o.map([](int &n) {return n * 2;});
+        static_assert(std::is_same_v<option<int>, decltype(r)>);
+        ASSERT_EQ(5, *o);
+        ASSERT_EQ(10, *r);
+    }
+    
+    TEST(OptionTest, RefMapConstLValueNullopt) {
+        const option<int &> o {};
+        ASSERT_EQ(nullopt, o.map([](int &n) {return n * 2;}));
+    }
+    
+    TEST(OptionTest, RefMapRValue) {
+        int x = 7;
+        decltype(auto) r = option<int &>(x).map([](int &n) {return n * 3;});
+        static_assert(std::is_same_v<option<int>, decltype(r)>);
+        ASSERT_EQ(21, *r);
+    }
+    
+    TEST(OptionTest, RefMapRValueNullopt) {
+        ASSERT_EQ(nullopt, option<int &> {}.map([](int &n) {return n * 3;}));
+    }
+    
+    TEST(OptionTest, MapReturnRef) {
+        std::vector<int> v {10, 20, 30};
+        decltype(auto) r = option<int>(0).map([&v](int i) -> decltype(auto) {return v[i];});
+        static_assert(std::is_same_v<option<int &>, decltype(r)>);
+        ASSERT_EQ(&v[0], &*r);
+    }
 }

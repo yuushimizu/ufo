8c08a379d6ce3ccb5c2e23bb0e43f3a21aba94e4
Yuu Shimizu <p@yuushimizu.com>
2017-10-16 22:46:39 +0900

    add get

--- Test/container.cpp
+++ Test/container.cpp
@@ -3,8 +3,45 @@
 #include "ufo/placeholder.hpp"
 #include <vector>
 #include <map>
+#include "TD.hpp"
 
 using namespace ufo;
 
 namespace {
+    TEST(ContainerTest, Remove) {
+        std::vector<int> v {1, 2, 3, 4, 5};
+        remove(v, _ == 3);
+        ASSERT_EQ((std::vector<int> {1, 2, 4, 5}), v);
+    }
+    
+    TEST(ContainerTest, RemoveNotMatched) {
+        std::vector<int> v {1, 2, 3, 4};
+        remove(v, _ == 8);
+        ASSERT_EQ((std::vector<int> {1, 2, 3, 4}), v);
+    }
+    
+    TEST(ContainerTest, Get) {
+        std::map<int, std::string> m {{2, "two"}, {4, "four"}, {5, "five"}};
+        auto r = get(m, 4);
+        static_assert(std::is_same_v<option<std::string &>, decltype(r)>);
+        ASSERT_EQ(&m.at(4), &*r);
+    }
+    
+    TEST(ContainerTest, GetNotMatched) {
+        std::map<int, std::string> m {{1, "one"}, {2, "two"}};
+        ASSERT_EQ(nullopt, get(m, 4));
+    }
+    
+    TEST(ContainerTest, GetConst) {
+        const std::map<int, std::string> m {{1, "one"}, {3, "three"}};
+        auto r = get(m, 1);
+        static_assert(std::is_same_v<option<const std::string &>, decltype(r)>);
+        ASSERT_EQ(&m.at(1), &*r);
+    }
+    
+    TEST(ContainerTest, GetRValue) {
+        std::map<int, std::string> m {{1, "one"}};
+        static_assert(std::is_same_v<option<std::string>, decltype(get(std::move(m), 1))>);
+        ASSERT_EQ(std::string("one"), *get(std::move(m), 1));
+    }
 }
--- ufo/container.hpp
+++ ufo/container.hpp
@@ -1,58 +1,25 @@
 #ifndef ufo_container
 #define ufo_container
 
-#include <algorithm>
 #include "iterator.hpp"
 #include "option.hpp"
 
 namespace ufo {
-    template <typename Range, typename F>
-    constexpr auto find_iterator(Range &&range, F &&predicate) {
-        return std::find_if(adl_begin(range), adl_end(range), std::forward<F>(predicate));
-    }
-    
-    template <typename Range, typename F>
-    constexpr bool contains(const Range &range, F &&predicate) {
-        return find_iterator(range, std::forward<F>(predicate)) != adl_end(range);
-    }
-    
-    template <typename Range, typename Iterator>
-    constexpr auto iterator_optional(Range &&range, Iterator iterator) -> option<iterator_value_type_t<Iterator>> {
-        if (iterator == adl_end(range)) return nullopt;
-        return *iterator;
-    }
-    
-    template <typename Range, typename Iterator>
-    constexpr auto iterator_optref(Range &&range, Iterator iterator) -> option<std::remove_reference_t<decltype(*iterator)> &> {
-        if (iterator == adl_end(range)) return nullopt;
-        return *iterator;
-    }
-
-    template <typename Range, typename F>
-    constexpr auto find(const Range &range, F &&predicate) {
-        return iterator_optional(range, find_iterator(range, std::forward<F>(predicate)));
-    }
-    
-    template <typename Range, typename F>
-    auto find_ref(Range &&range, F &&predicate) {
-        return iterator_optref(range, find_iterator(range, std::forward<F>(predicate)));
-    }
-    
     template <typename Container, typename F>
     void remove(Container &container, F &&predicate) {
         container.erase(std::remove_if(adl_begin(container), adl_end(container), std::forward<F>(predicate)), adl_end(container));
     }
     
     template <typename Map, typename Key>
-    constexpr auto get(const Map &map, const Key &key) -> option<typename Map::mapped_type> {
+    constexpr auto get(Map &map, const Key &key) -> option<decltype(map.at(key))> {
         if (map.find(key) == adl_end(map)) return nullopt;
-        return map.at(key);
+        return std::forward<Map>(map).at(key);
     }
     
     template <typename Map, typename Key>
-    constexpr auto get_ref(Map &&map, const Key &key) -> option<std::remove_reference_t<decltype(map.at(key))> &> {
-        if (map.find(key) == adl_end(map)) return nullopt;
-        return map.at(key);
+    constexpr auto get(Map &&map, const Key &key) -> option<std::remove_reference_t<decltype(map.at(key))>> {
+        if (map.find(key) == adl_end(map)) return nullptr;
+        return std::move(map.at(key));
     }
 }
 

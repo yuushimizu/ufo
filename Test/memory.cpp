60a2b2cb35c88d4e63ae6586c046e92561a1b088
Yuu Shimizu <p@yuushimizu.com>
2017-10-27 15:19:05 +0900

    add owner_before_compare

--- Test/memory.cpp
+++ Test/memory.cpp
@@ -1,5 +1,6 @@
 #include <gtest/gtest.h>
 #include <memory>
+#include <set>
 #include "ufo/memory.hpp"
 
 using namespace ufo;
@@ -16,5 +17,12 @@ namespace {
             ASSERT_EQ(5, *p);
         }
         ASSERT_EQ(1, count);
+    }
+    
+    TEST(OwnerBeforeCompareTest, Set) {
+        auto shared = std::make_shared<int>(10);
+        std::set<std::shared_ptr<int>, owner_before_compare> s {};
+        s.insert(shared);
+        ASSERT_EQ(1, s.count(shared));
     }
 }
--- ufo/memory.hpp
+++ ufo/memory.hpp
@@ -8,6 +8,13 @@ namespace ufo {
     auto make_unique_ptr(P *p, D deleter) noexcept {
         return std::unique_ptr<P, D>(p, std::move(deleter));
     }
+    
+    struct owner_before_compare {
+        template <typename LHS, typename RHS>
+        bool operator()(const LHS &lhs, const RHS &rhs) const noexcept {
+            return lhs.owner_before(rhs);
+        }
+    };
 }
 
 #endif

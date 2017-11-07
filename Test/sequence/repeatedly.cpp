614f70e55ac968baf595c6a7dddb71862453bb7e
Yuu Shimizu <p@yuushimizu.com>
2017-11-08 02:07:30 +0900

    add repeatedly

---
+++ Test/sequence/repeatedly.cpp
@@ -0,0 +1,27 @@
+#include <gtest/gtest.h>
+#include "ufo/sequence/repeatedly.hpp"
+#include <type_traits>
+#include "ufo/option.hpp"
+#include "delete_copy.hpp"
+
+using namespace ufo;
+
+namespace {
+    TEST(RepeatedlyTest, Repeatedly) {
+        auto r = repeatedly([x = 0]() mutable {
+            return make_option(x++);
+        });
+        static_assert(std::is_same_v<option<int>, decltype(r.next())>);
+        ASSERT_EQ(0, *r.next());
+        ASSERT_EQ(1, *r.next());
+        ASSERT_EQ(2, *r.next());
+        ASSERT_EQ(3, *r.next());
+    }
+    
+    TEST(RepeatedlyTest, FunctionNotCopied) {
+        auto r = repeatedly(test::delete_function_copy([]() {return make_option(42);}));
+        ASSERT_EQ(42, *r.next());
+        ASSERT_EQ(42, *r.next());
+        ASSERT_EQ(42, *r.next());
+    }
+}
---
+++ ufo/sequence/repeatedly.hpp
@@ -0,0 +1,29 @@
+#ifndef ufo_sequence_repeatedly
+#define ufo_sequence_repeatedly
+
+#include "sequence.hpp"
+#include "../option.hpp"
+#include "../LambdaWrapper.hpp"
+
+namespace ufo {
+    template <typename F>
+    class Repeatedly : public sequence {
+    private:
+        LambdaWrapper<F> f_;
+        
+    public:
+        constexpr explicit Repeatedly(F f) noexcept : f_(std::move(f)) {
+        }
+        
+        constexpr auto next() {
+            return f_();
+        }
+    };
+    
+    template <typename F>
+    constexpr auto repeatedly(F f) noexcept {
+        return Repeatedly<F>(std::move(f));
+    }
+}
+
+#endif

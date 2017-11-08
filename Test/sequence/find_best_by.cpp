338a09b2075cd702e49c261b19c61aa35bfcbdfe
Yuu Shimizu <p@yuushimizu.com>
2017-11-08 11:50:45 +0900

    add find_best_by

--- Test/sequence/find_best_by.cpp
+++ Test/sequence/find_best_by.cpp
@@ -1,5 +1,5 @@
 #include <gtest/gtest.h>
-#include "ufo/sequence/max_by.hpp"
+#include "ufo/sequence/find_best_by.hpp"
 #include <vector>
 #include <type_traits>
 #include <memory>
@@ -9,41 +9,41 @@
 using namespace ufo;
 
 namespace {
-    TEST(MaxByTest, LValue) {
+    TEST(FindBestByTest, LValue) {
         std::vector<int> v {13, 48, 91, 26, 59, 33};
         auto cw = container_wrapper(v);
-        decltype(auto) r = cw | max_by(_ % 10);
+        decltype(auto) r = cw | find_best_by(_ % 10, [](int n, int m) {return n > m;});
         static_assert(std::is_same_v<option<int &>, decltype(r)>);
         ASSERT_EQ(&v[4], &*r);
     }
     
-    TEST(MaxByTest, CopiedLValueNotChanged) {
+    TEST(FindBestByTest, CopiedLValueNotChanged) {
         auto cw = container_wrapper(std::vector<int> {19, 22, 35});
-        auto r = cw | max_by(_ % 10);
-        ASSERT_EQ(19, *r);
+        auto r = cw | find_best_by(_ % 10, [](int n, int m) {return n < m;});
+        ASSERT_EQ(22, *r);
         ASSERT_EQ(19, *cw.next());
         ASSERT_EQ(22, *cw.next());
         ASSERT_EQ(35, *cw.next());
         ASSERT_FALSE(cw.next());
     }
     
-    TEST(MaxByTest, RValue) {
-        decltype(auto) r = std::vector<int> {40, 13, 24} | max_by(-_);
+    TEST(FindBestByTest, RValue) {
+        decltype(auto) r = std::vector<int> {40, 13, 24} | find_best_by(-_, [](int n, int m) {return n > m;});
         static_assert(std::is_same_v<option<int>, decltype(r)>);
         ASSERT_EQ(13, *r);
     }
     
-    TEST(MaxByTest, Empty) {
-        auto r = std::vector<int> {} | max_by(_ % 10);
+    TEST(FindBestByTest, Empty) {
+        auto r = std::vector<int> {} | find_best_by(_ % 10, [](int n, int m) {return n < m;});
         ASSERT_FALSE(r);
     }
-    
-    TEST(MaxByTest, OneValue) {
-        auto r = std::vector<int> {56} | max_by(-_);
+
+    TEST(FindBestByTest, OneValue) {
+        auto r = std::vector<int> {56} | find_best_by(_ % 5, [](auto n, auto m) {throw "unreached"; return true;});
         ASSERT_EQ(56, *r);
     }
-    
-    TEST(MaxByTest, ReferenceKeyNotCopied) {
+
+    TEST(FindBestByTest, ReferenceKeyNotCopied) {
         struct Uncopyable {
         public:
             int value;
@@ -66,21 +66,13 @@ namespace {
                 other.value = 0;
                 return *this;
             }
-            
-            bool operator<(const Uncopyable &other) {
-                return value < other.value;
-            }
-            
-            bool operator>(const Uncopyable &other) {
-                return value > other.value;
-            }
         };
         std::vector<Uncopyable> values {};
         values.emplace_back(20);
         values.emplace_back(40);
         values.emplace_back(30);
         values.emplace_back(10);
-        auto r = std::vector<int> {3, 0, 1, 2} | max_by([&values](int n) -> decltype(auto) {return values[n];});
+        auto r = std::vector<int> {3, 0, 1, 2} | find_best_by([&values](int n) -> decltype(auto) {return values[n];}, [](auto &x, auto &y) {return x.value > y.value;});
         ASSERT_EQ(1, *r);
         ASSERT_EQ(20, values[0].value);
         ASSERT_EQ(40, values[1].value);
@@ -88,13 +80,13 @@ namespace {
         ASSERT_EQ(10, values[3].value);
     }
     
-    TEST(MaxByTest, SequenceNotCopied) {
-        auto r = std::vector<int> {2, 3, 4} | test::delete_copy | max_by([](auto n) {return n;});
+    TEST(FindBestByTest, SequenceNotCopied) {
+        auto r = std::vector<int> {2, 3, 4} | test::delete_copy | find_best_by([](auto n) {return n;}, [](auto &x, auto &y) {return x > y;});
         ASSERT_EQ(4, *r);
     }
     
-    TEST(MaxByTest, FunctionNotCopied) {
-        auto r = std::vector<int> {2, 3, 4} | max_by(test::delete_function_copy(-_));
+    TEST(FindBestByTest, FunctionNotCopied) {
+        auto r = std::vector<int> {2, 3, 4} | find_best_by(test::delete_function_copy([](auto n) {return -n;}), test::delete_function_copy([](auto &x, auto &y) {return x > y;}));
         ASSERT_EQ(2, *r);
     }
 }
--- ufo/sequence/max_by.hpp
+++ ufo/sequence/max_by.hpp
@@ -1,14 +1,13 @@
 #ifndef ufo_sequence_max_by
 #define ufo_sequence_max_by
 
+#include <functional>
 #include "find_best_by.hpp"
 
 namespace ufo {
     template <typename F>
     constexpr auto max_by(F &&f) {
-        return find_best_by(std::forward<F>(f), [](auto &&x, auto &&y) {
-            return std::forward<decltype(x)>(x) > std::forward<decltype(y)>(y);
-        });
+        return find_best_by(std::forward<F>(f), std::greater<> {});
     }
 }
 
